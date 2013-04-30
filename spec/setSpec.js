77aadb51a2956cb2560cdeac27b13dd0b5488c0e
Hack Reactor Students <github@hackreactor.com>
2013-04-30 11:11:07 -0700

    wrote tests for set and implemented the data structure

--- spec/setSpec.js
+++ spec/setSpec.js
@@ -10,4 +10,45 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  describe("#add", function(){
+    it("should add strings to the set", function(){
+      set.add("a");
+      expect(set._storage[0]).toEqual("a");
+    });
+    it("should not add a duplicate element to the set", function(){
+      set.add("a");
+      set.add("a");
+      var count = 0;
+      for (var i = 0; i < set._storage.length; i++){
+        if(set._storage[i] === "a"){
+          count++;
+        }
+      }
+      expect(count).toEqual(1);
+    });
+  });
+  describe("#contains", function(){
+    beforeEach(function(){
+      set.add("a");
+      set.add("b");
+      set.add("c");
+    });
+    it("should contain added strings", function(){
+      expect(set.contains("a")).toEqual(true);
+      expect(set.contains("b")).toEqual(true);
+      expect(set.contains("c")).toEqual(true);
+    });
+    it("should not contain other strings", function(){
+      expect(set.contains("d")).toEqual(false);
+    });
+  });
+  describe('#remove', function(){
+    it("should remove an element from the set", function(){
+      set.add("a");
+      set.remove("a");
+      expect(set.contains("a")).toEqual(false);
+    });
+  });
+
 });
--- src/set.js
+++ src/set.js
@@ -1,16 +1,24 @@
 var makeSet = function(){
-  var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  var set = Object.create(setPrototype);
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(value){
+  if(this._storage.indexOf(value)=== -1){
+    this._storage.push(value);
+  }
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(value){
+  return this._storage.indexOf(value) !== -1;
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(value){
+  var x =this._storage.indexOf(value);
+  if (x !==-1) {
+    this._storage.splice(x,1);
+  }
 };
