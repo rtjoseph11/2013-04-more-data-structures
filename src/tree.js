dcba5030631cc861816c231145da33a8651ceaad
Hack Reactor Students <github@hackreactor.com>
2013-04-29 18:27:25 -0700

    started implementing tree structure

--- src/tree.js
+++ src/tree.js
@@ -1,15 +1,29 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
-
+  newTree.value = value;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var node = makeTree(value);
+  this.children.push(node);
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(value, result){
+  result = result || false;
+  if (this.value === value){
+    result = true;
+  }
+  else if (this.children.length){
+    _.each(children, function(item){
+      if (!result){
+        result = item.contains(value, result);
+      }
+    });
+  }
+  return result;
 };
