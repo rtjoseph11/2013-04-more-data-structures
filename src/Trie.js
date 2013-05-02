c8e6acb414d16403025862835f83c89a0f2922aa
Nick Miller <njmiller6@gmail.com>
2013-05-02 14:18:14 -0700

    Trie spec and Trie class stub

---
+++ src/Trie.js
@@ -0,0 +1,20 @@
+var Trie = function(strValue){
+	this.value = strValue ? strValue : '';
+	this.children = [];
+};
+
+Trie.prototype.insert = function(){
+
+};
+
+Trie.prototype.contains = function(){
+	
+};
+
+Trie.prototype.remove = function(){
+	
+};
+
+Trie.prototype.getNexLetters = function(strValue){
+	
+};
